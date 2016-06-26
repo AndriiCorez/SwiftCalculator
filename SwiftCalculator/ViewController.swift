0381dd1e6ab1a3ae6ce3d91738eb151f51a5eb0e
Corezina <corezina@corezinas-imac.local>
2016-06-26 12:01:57 -0700

    Buttons are added along with its delegates

--- SwiftCalculator/ViewController.swift
+++ SwiftCalculator/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var isUserIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+
+        if isUserIsInTheMiddleOfTyping{
+            let textCurrentlyDisplayed = display.text!
+            display.text = textCurrentlyDisplayed + digit
+        } else {
+            display.text = digit
+        }
+        
+        isUserIsInTheMiddleOfTyping = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func performOperation(sender: UIButton) {
+        isUserIsInTheMiddleOfTyping = false
+        
+        if let mathSymbol = sender.currentTitle{
+            if mathSymbol == "Pi"{
+                display.text = String(M_PI)
+            }
+        }
     }
-
-
 }
 
